1d71a7647c2cbd8cd42cc1567883b0fdce3b3388
Niklas Kemm <niklas.kemm@googlemail.com>
2020-04-28 15:14:14 +0200

    cleaning up & integrated register form

---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h3>REGISTER</h3>
+
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input 
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthentificationService from '@/services/AuthentificationService'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthentificationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }  
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
